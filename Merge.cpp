d926f837d0bcf787f71afe482ad706bfde2b2387
Gardian <gardianrahman91@gmail.com>
2025-04-17 18:22:22 +0700

    Step 4

--- Merge.cpp
+++ Merge.cpp
@@ -7,11 +7,18 @@ int n;
 void mergeSort(int low, int high)
 {
     if (low>=high)
-    {
-        return;
+    {           // step 1
+        return; // step 1a
     }
-    int mid = (low+high) / 2;
+    int mid = (low+high) / 2; // step 2
+
+    mergeSort(low,mid); // step 3a
+    mergeSort(mid + 1, high); // step 3b
+
+    int i = low; // step 4a
+    int j,k;
+    j = mid + 1; // step 4b
+    k + low; // step 4c
+
     
-    mergeSort(low,mid);
-    mergeSort(mid + 1, high);
 }
